a82d0a543c4a733f2fe734f7fd891055c8f35adb
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-04-29 09:50:13 +0900

    Add: api 연동

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import React from "react";
-import {Router} from "react-router-dom";
+import React, {useState, useEffect} from "react";
+// import {Router} from "react-router-dom";
 import {GlobalStyle} from "./GlobalStyle";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
+import Repo from "./components/Repo/Repo";
 
-function App() {
+function App(props: any) {
+  const [repo, setRepo] = useState([]);
+  const [loading, setLoading] = useState(true); //loading중일때
+  const [user, setUser]: any = useState("");
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/seoyeon-baek/repos`, {
+      headers: {Authorization: ""},
+    })
+      .then(res => res.json()) //json형태로 변환
+      .then(data => {
+        setRepo(data);
+        setLoading(false);
+      });
+  }, [user]);
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header user={user} onSearch={setUser} />
       <Nav />
       <Profile />
+      <Repo repo={repo} />
     </>
   );
 }
