174883a2499407a149dc3c79d4504076166f2a16
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-04-30 17:43:27 +0900

    Add: Profile layout

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import * as S from "./style";
+import Follow from "../../assets/svgfiles/follow.svg";
 
 const Profile = ({user}: {user: any}) => {
   return (
     <>
       <S.Wrapper>
-        <S.ProfileImg></S.ProfileImg>
+        <S.ProfileImg />
+        <S.UserWrapper>
+          <S.Name>백서연</S.Name>
+          <S.Ename>seoyeon-baek</S.Ename>
+          <textarea id="description" max-length="160" readOnly>
+            안녕하십니까
+          </textarea>
+        </S.UserWrapper>
+        <S.FollowWrapper>
+          <img src={Follow} />
+          <span className="num">53</span>
+          <span className="follow">followers ·</span>
+          <span className="num">53</span>
+          <span className="follow">following</span>
+        </S.FollowWrapper>
       </S.Wrapper>
     </>
   );
--- src/components/Profile/style.ts
+++ src/components/Profile/style.ts
@@ -4,14 +4,58 @@ export const Wrapper = styled.div`
   position: absolute;
   width: 440px;
   height: 100%;
+  margin-left: 110px;
+
+  #description {
+    width: 296px !important;
+    height: auto;
+    /* background-color: red; */
+    border: none;
+    resize: none;
+    margin-top: 18px;
+    font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    font-size: 16px;
+    overflow: hidden;
+    /* margin-bottom: 10px; */
+    font-weight: 400;
+    outline: none;
+  }
 `;
 
-export const ProfileImg = styled.div`
-  position: absolute;
+export const ProfileImg = styled.img`
+  /* position: absolute; */
   width: 296px;
   height: 296px;
   background-color: red;
   border-radius: 100%;
-  top: -30px;
-  left: 110px;
+  margin-top: -30px;
+  margin-bottom: 17px;
+`;
+
+export const UserWrapper = styled.div``;
+export const Name = styled.h2`
+  margin-bottom: 4px;
+  font-size: 24px;
+  font-weight: 600;
+`;
+export const Ename = styled.h3`
+  color: #57606a;
+  font-size: 20px;
+`;
+export const FollowWrapper = styled.div`
+  img {
+    vertical-align: middle;
+  }
+
+  .num {
+    font-weight: 700;
+    font-size: 14px;
+    margin-left: 6px;
+  }
+
+  .follow {
+    font-size: 14px;
+    margin-left: 4px;
+    color: #57606a;
+  }
 `;
